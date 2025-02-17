266245525614312ba520a3b9525d2a28897d389a
remi-remi <remiveritepro@gmail.com>
2025-02-17 13:24:40 +0100

    modified:   lua/settings.lua

--- lua/settings.lua
+++ lua/settings.lua
@@ -8,7 +8,14 @@ vim.opt.number = true
 vim.opt.relativenumber = false
 
 -- Set encoding
-vim.opt.encoding = 'utf-8'
+vim.opt.encoding = 'utf-8'      -- Définit l'encodage de Neovim
+vim.opt.fileencoding = 'utf-8'  -- Définit l'encodage des fichiers
+
+vim.opt.expandtab = true      -- Remplace les tabs par des espaces
+vim.opt.shiftwidth = 3        -- Définit l'indentation automatique à X espaces
+vim.opt.softtabstop = 3       -- Nombre d'espaces quand on appuie sur TAB
+vim.opt.tabstop = 3           -- Nombre d'espaces qu'un tab représente
+
 
 -- Enable syntax highlighting
 vim.cmd('syntax on')
