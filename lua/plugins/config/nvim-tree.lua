38796e172c5aaa77c4db416d92fa2700a511de59
remi-remi <remiveritepro@gmail.com>
2025-08-19 10:12:54 +0200

    Changes to be committed: 	modified:   config/nvim-tree.lua 	modified:   ../../lazy-lock.json 	modified:   ../core/settings.lua 	modified:   ../core/wl-tools.lua 	modified:   ../keymaps/plugins.lua
    reconfiguration

--- lua/plugins/config/nvim-tree.lua
+++ lua/plugins/config/nvim-tree.lua
@@ -1,81 +1,82 @@
 return {
-  update_focused_file = {
-    enable = true,
-    update_root = false,
-  },
-  disable_netrw = true,
-  hijack_netrw = true,
-  hijack_cursor = true,
-  update_cwd = true,
+   update_focused_file = {
+      enable = true,
+      update_root = false,
+   },
+   disable_netrw = true,
+   hijack_netrw = true,
+   hijack_cursor = true,
+   update_cwd = true,
 
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-
-  view = {
-    width = {min = 10, max = 40},
-    adaptive_size = true,
-    side = 'left',
-  },
-
-  renderer = {
-  highlight_opened_files = "all",
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_closed = "",
-          arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "✗",
-          staged = "✓",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "★",
-          deleted = "",
-          ignored = "◌",
-        },
+   diagnostics = {
+      enable = true,
+      icons = {
+         hint = "",
+         info = "",
+         warning = "",
+         error = "",
       },
-    },
-  },
+   },
 
-  git = {
-    enable = true,
-    ignore = false,
-  },
+   view = {
+      number = true,
+      width = { min = 10, max = 40 },
+      adaptive_size = true,
+      side = 'left',
+   },
 
-  on_attach = function(bufnr)
-    local api = require('nvim-tree.api')
+   renderer = {
+      highlight_opened_files = "all",
+      icons = {
+         glyphs = {
+            default = "",
+            symlink = "",
+            folder = {
+               arrow_closed = "",
+               arrow_open = "",
+               default = "",
+               open = "",
+               empty = "",
+               empty_open = "",
+               symlink = "",
+               symlink_open = "",
+            },
+            git = {
+               unstaged = "✗",
+               staged = "✓",
+               unmerged = "",
+               renamed = "➜",
+               untracked = "★",
+               deleted = "",
+               ignored = "◌",
+            },
+         },
+      },
+   },
 
-    local function opts(desc)
-      return {
-        desc = 'nvim-tree: ' .. desc,
-        buffer = bufnr,
-        noremap = true,
-        silent = true,
-        nowait = true,
-      }
-    end
+   git = {
+      enable = true,
+      ignore = false,
+   },
 
-    -- Default mappings (optional)
-    api.config.mappings.default_on_attach(bufnr)
+   on_attach = function(bufnr)
+      local api = require('nvim-tree.api')
 
-    -- Custom mappings
-    vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
-    vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
-  end,
+      local function opts(desc)
+         return {
+            desc = 'nvim-tree: ' .. desc,
+            buffer = bufnr,
+            noremap = true,
+            silent = true,
+            nowait = true,
+         }
+      end
+
+      -- Default mappings (optional)
+      api.config.mappings.default_on_attach(bufnr)
+
+      -- Custom mappings
+      vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+      vim.keymap.set('n', 's', api.node.open.horizontal, opts('Open: Horizontal Split'))
+   end,
 }
