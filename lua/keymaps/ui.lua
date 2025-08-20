6863f67799929d661daba1cc42d6374119da9ed8
remi-remi <remiveritepro@gmail.com>
2025-08-20 16:20:25 +0200

    terminalKeyConfiguration 	modified:   lua/keymaps/ui.lua
    the term is amazing, use :terminal to launche, "i" to write, <esc> to go
    to normal mode, and use it like a normal buffer !

--- lua/keymaps/ui.lua
+++ lua/keymaps/ui.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', '<C-A-Right>', ':tabnext<CR>', { noremap = true, silent = fa
 vim.keymap.set('n', '<C-A-Left>', ':tabprevious<CR>', { noremap = true, silent = false })
 vim.keymap.set('n', '<Leader>n', ':tabnew<CR>', { noremap = true, silent = true })
 
+-- exit from terminal mode with the same behavior than any other modes
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true, desc = "Exit terminal mode" })
