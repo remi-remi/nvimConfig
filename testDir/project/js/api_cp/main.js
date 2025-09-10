4d4fae08d9144335e31089f733485ab97e999bbb
remi-remi <remiveritepro@gmail.com>
2025-09-10 23:53:12 +0200

    fix/disableGostText 	modified:   lazy-lock.json 	modified:   lua/plugins/completion.lua 	modified:   lua/plugins/config/completion.lua 	modified:   testDir/project/js/api_cp/main.js

--- testDir/project/js/api_cp/main.js
+++ testDir/project/js/api_cp/main.js
@@ -1,5 +1,7 @@
 import { functionToExport } from './dir/functionToExport.js'
 import funckyName from "./dir/file2.js"
+import { functionToExport } from './dir/functionToExport.js'
+
 
 const test = () => {
    // const zazef = "aser"
