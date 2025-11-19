d6455389a6b96ed128d0415b91a40f4a98ddfca0
remi-remi <remiveritepro@gmail.com>
2025-11-19 12:07:20 +0100

    feature/nvim-split-shortcuts 	modified:   lua/keymaps/init.lua 	new file:   lua/keymaps/split.lua

---
+++ lua/keymaps/split.lua
@@ -0,0 +1,13 @@
+vim.keymap.set('n', '<leader><left>', '<C-w>h')
+vim.keymap.set('n', '<leader><down>', '<C-w>j')
+vim.keymap.set('n', '<leader><up>', '<C-w>k')
+vim.keymap.set('n', '<leader><right>', '<C-w>l')
+
+
+vim.keymap.set('n', '<leader>h', ':split<CR>')
+vim.keymap.set('n', '<leader>v', ':vsplit<CR>')
+
+vim.keymap.set('n', '<leader><S-left>', ':vertical resize -3<CR>')
+vim.keymap.set('n', '<leader><S-right>', ':vertical resize +3<CR>')
+vim.keymap.set('n', '<leader><S-up>', ':resize -2<CR>')
+vim.keymap.set('n', '<leader><S-down>', ':resize +2<CR>')
