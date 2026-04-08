692a24c3dbd491c9c8ab9281b84aa1a500ea1cc1
remi-remi <remiveritepro@gmail.com>
2026-04-09 01:23:47 +0200

    imperfect but will allow to use hover doc Changes to be committed: 	modified:   lazy-lock.json 	modified:   lua/plugins/treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,12 @@
--- treesitter.lua
 return {
    {
       "nvim-treesitter/nvim-treesitter",
       build = ":TSUpdate",
+      lazy = true, -- ne charge jamais le plugin au runtime
+      cmd = { "TSInstall", "TSUpdate", "TSInstallInfo" },
       config = function()
          require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "vim", "bash", "javascript", "typescript", "tsx", "html", "css", "json", "markdown", "sql" },
+            ensure_installed = { "lua", "vim", "bash", "javascript", "typescript", "tsx", "html", "css", "json", "markdown", "sql", "markdown_inline" },
             highlight = { enable = false },
             indent = { enable = false },
          })
@@ -17,7 +18,7 @@ return {
       event = "FileType",
       config = function()
          vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "lua", "vim", "bash", "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "json", "markdown", "SQL" },
+            pattern = { "lua", "vim", "bash", "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "json", "markdown", "SQL", "markdown_inline" },
             callback = function()
                local ok, err = pcall(vim.treesitter.start)
                if not ok then
