93a063bf476b14c34880ff916846c0ea21cebfb4
remi-remi <remiveritepro@gmail.com>
2026-04-14 11:04:51 +0200

    went back to legacy treesitter the legacy version still work proprely and because arch ship treesitter with a too old ABI migrating to the current version of treesitter is impossible on my arch configuration,
    this update warn arch user if they use nvim > 0.12
    and advice a rollback
    
    Changes to be committed:
    	modified:   lazy-lock.json
    	modified:   lua/plugins/treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,31 +1,60 @@
 return {
-   {
-      "nvim-treesitter/nvim-treesitter",
-      build = ":TSUpdate",
-      lazy = true, -- ne charge jamais le plugin au runtime
-      cmd = { "TSInstall", "TSUpdate", "TSInstallInfo" },
-      config = function()
-         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "vim", "bash", "javascript", "typescript", "tsx", "html", "css", "json", "markdown", "sql", "markdown_inline" },
-            highlight = { enable = false },
-            indent = { enable = false },
-         })
-      end,
-   },
-   {
-      name = "treesitter-start",
-      dir = vim.fn.stdpath("config"),
-      event = "FileType",
-      config = function()
-         vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "lua", "vim", "bash", "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "css", "json", "markdown", "SQL", "markdown_inline" },
-            callback = function()
-               local ok, err = pcall(vim.treesitter.start)
-               if not ok then
-                  vim.notify("Treesitter: " .. err, vim.log.levels.WARN)
-               end
-            end,
-         })
-      end,
-   },
+	"nvim-treesitter/nvim-treesitter",
+	branch = "master",
+	lazy = false,
+	build = ":TSUpdate",
+	-- Détection Arch Linux seulement
+	config = function()
+		local is_arch = vim.fn.executable("pacman") == 1
+		local version = vim.version()
+		local major, minor = version.major, version.minor
+
+		-- Condition corrigée (> 0.11.x)
+		if is_arch and (major > 0 or (major == 0 and minor > 11)) then
+			vim.api.nvim_echo({
+				{
+					"⚠️  Neovim v" .. major .. "." .. minor .. "." .. version.patch .. " detected",
+					"WarningMsg",
+				},
+				{ " → Downgrade to 0.11.6 required!", "ErrorMsg" },
+				{ " (Arch ship tree-sitter with ABI-v14, nvim 0.12 needs ABIv15, breaking)", "Normal" },
+				{ "\nPress [ENTER] to continue anyway or [q] to quit...", "Comment" },
+			}, false, {}) -- false = pas history
+
+			-- BLOQUE jusqu'à touche
+			vim.fn.getchar()
+			vim.cmd("redraw!") -- Nettoie écran
+		end
+
+		vim.opt.runtimepath:prepend(vim.fn.stdpath("data") .. "/lazy/nvim-treesitter")
+
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = {
+				"markdown",
+				"markdown_inline",
+				"lua",
+				"javascript",
+				"typescript",
+				"tsx",
+				"json",
+				"jsonc",
+				"html",
+				"css",
+				"scss",
+				"yaml",
+				"bash",
+				"sql",
+				"dockerfile",
+			},
+			highlight = { enable = true },
+			textobjects = { enable = true },
+		})
+
+		vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+			pattern = { "markdown", "lsp_float" },
+			callback = function()
+				vim.treesitter.start(0, "markdown")
+			end,
+		})
+	end,
 }
