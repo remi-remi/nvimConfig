17e1a8dbed06a17d4c68583b9b811ce4c247989c
remi-remi <remiveritepro@gmail.com>
2026-04-14 11:33:43 +0200

    bugfix: no lsp when opening file directly Changes to be committed: 	modified:   lua/lsp/init.lua

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,15 +1,19 @@
 local lsps = {
-   { "bashls", {} },
-   { "lua_ls", require("lsp.lua") },
-   { "cssls",  require("lsp.css") },
-   { "eslint", require("lsp.eslint") },
-   { "vtsls",  require("lsp.js") },
+	{ "bashls", {} },
+	{ "lua_ls", require("lsp.lua") },
+	{ "cssls", require("lsp.css") },
+	{ "eslint", require("lsp.eslint") },
+	{ "vtsls", require("lsp.js") },
 }
 
 for _, lsp in ipairs(lsps) do
-   local name, config = lsp[1], lsp[2]
-   if config then
-      vim.lsp.config(name, config)
-   end
-   vim.lsp.enable(name)
+	local name, config = lsp[1], lsp[2]
+	if config then
+		vim.lsp.config(name, config)
+	end
+	vim.lsp.enable(name)
 end
+
+-- when a file is directly open, .enable happen too late to detect the filetype
+-- fixed by checking the filetype after configuration
+vim.cmd("filetype detect")
