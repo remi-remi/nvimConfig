1ee120a64fac471fe5d093fc860e6ee2a079dbb8
remi-remi <remiveritepro@gmail.com>
2026-04-27 13:00:52 +0200

    try docker LSP (unfinished) Changes to be committed: 	modified:   lua/lsp/init.lua 	modified:   lua/plugins/mason.lua

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,4 +1,6 @@
 local lsps = {
+	{ "dockerls", { cmd = { "docker-langserver", "--stdio" } } },
+	{ "docker_compose_language_service", {} },
 	{ "bashls", {} },
 	{ "lua_ls", require("lsp.lua") },
 	{ "cssls", require("lsp.css") },
@@ -19,6 +21,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		local client = vim.lsp.get_client_by_id(args.data.client_id)
 		vim.notify("LSP started: " .. client.name, vim.log.levels.INFO)
 	end,
+})
+
+-- to move to docker_compose_language_service latter
+vim.filetype.add({
+	filename = {
+		["docker-compose.yml"] = "yaml.docker-compose",
+		["docker-compose.yaml"] = "yaml.docker-compose",
+		["compose.yml"] = "yaml.docker-compose",
+		["compose.yaml"] = "yaml.docker-compose",
+	},
 })
 
 -- when a file is directly open, .enable happen too late to detect the filetype
